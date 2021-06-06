1e2d5f8974534c41a4ecabb5cf7fa3fcbca3f18c
MrFabulous00 <dudrbs10400@gmail.com>
2021-06-06 22:36:05 +0900

    중간 커밋

---
+++ ToyProject5/main.js
@@ -0,0 +1,8 @@
+const items = document.querySelector("items");
+const input = document.querySelector("footerInput");
+const addBtn = document.querySelector("footerButton");
+
+function onAdd() {
+    // 1. 사용자가 입력한 텍스트를 받아옴
+    // 2. 새로운 아이템을 만듬 ()
+}
