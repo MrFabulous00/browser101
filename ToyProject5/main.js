cb53687cf7dc375bdbe422f3453313414b8d3926
MrFabulous00 <dudrbs10400@gmail.com>
2021-06-07 04:46:25 +0900

    자기전 커밋

--- ToyProject5/main.js
+++ ToyProject5/main.js
@@ -1,8 +1,59 @@
+"use strict";
+
 const items = document.querySelector("items");
 const input = document.querySelector("footerInput");
 const addBtn = document.querySelector("footerButton");
 
 function onAdd() {
     // 1. 사용자가 입력한 텍스트를 받아옴
-    // 2. 새로운 아이템을 만듬 ()
+    const text = input.value;
+    if (text === "") {
+        input.focus();
+        return;
+    }
+    console.log(text);
+    // 2. 새로운 아이템을 만듬 (텍스트 + 삭제 버튼)
+    const item = createItems();
+    // 3. items 컨테이너안에 새로 만든 아이템을 추가한다
+    items.appendChild(item);
+    // 4. 인풋을 초기화 한다.
+    input.value = "";
+    input.focus();
 }
+
+function createItem(text) {
+    const itemRow = document.createElement("li");
+    itemRow.setAttribute("class", "itemRow");
+
+    const item = document.createElement("div");
+    item.setAttribute("class", "item");
+
+    const name = document.createElement("span");
+    name.setAttribute("class", "itemName");
+    name.innerText = text;
+
+    const deleteBtn = document.createElement("div");
+    deleteBtn.setAttribute("class", "itemDelete");
+    deleteBtn.innerHTML = '<i class="fas fa-trash-alt"></i>';
+    deleteBtn.addEventListener("click", () => {
+        items.remove(itemLow);
+    });
+
+    const itemDivider = document.createElement("div");
+    itemDivider.setAttribute("class", "itemDivider");
+
+    item.appendChild(name);
+    item.appendChild(deleteBtn);
+
+    itemRow.appendChild(item);
+    itemRow.appendChild(itemDivider);
+    return itemRow;
+}
+
+addBtn.addEventListener("load", () => {
+    onAdd();
+});
+
+input.addEventListener("keypress", (e) => {
+    console.log("key");
+});
